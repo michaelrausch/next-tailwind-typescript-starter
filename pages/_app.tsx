342541d8dfc677f4ce5d6a76ddd09eed20a6af04
Michael Rausch <me@michaelrausch.nz>
2021-07-27 14:39:12 +1200

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <DefaultSeo
+      openGraph={{
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://www.url.ie/',
+        site_name: 'SiteName',
+      }}
+      twitter={{
+        handle: '@handle',
+        site: '@site',
+        cardType: 'summary_large_image',
+      }}
+      titleTemplate = 'Next | %s'
+    />
+    
+    <Component {...pageProps} />
+  </>
 }
 export default MyApp
